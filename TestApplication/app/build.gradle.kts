fcfd1d4376664cf3c588550af8b52c90540c6796
yucelutku <utkuyucel1903@gmail.com>
2025-09-22 23:54:47 +0300

    build: Gradle ve Android SDK sürümleri güncellendi; yeni bileşenler eklendi.

--- TestApplication/app/build.gradle.kts
+++ TestApplication/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.testapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.testapplication"
